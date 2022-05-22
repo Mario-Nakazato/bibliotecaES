6bf60a80eb6982190c205bbaae52e9600db66058
Mario Nakazato <marionakazat@gmail.com>
2022-05-22 03:04:24 -0300

    Criado Projeto Teste de Unidade

---
+++ biblioteca/bibliotecaTesteUnidade/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("bibliotecaTesteUnidade")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("bibliotecaTesteUnidade")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("2674d5e4-1e0a-4bce-9a5f-4711af002ad8")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
