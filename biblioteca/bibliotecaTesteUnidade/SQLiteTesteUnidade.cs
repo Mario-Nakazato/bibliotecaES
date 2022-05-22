d68ec9c6da45ad7b96abc45c0ef62c72d9429179
Mario Nakazato <marionakazat@gmail.com>
2022-05-22 03:17:34 -0300

    SQLite  teste unidade

--- biblioteca/bibliotecaTesteUnidade/SQLiteTesteUnidade.cs
+++ biblioteca/bibliotecaTesteUnidade/SQLiteTesteUnidade.cs
@@ -1,14 +1,84 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using biblioteca;
 using System;
+using System.Data;
 
 namespace bibliotecaTesteUnidade
 {
     [TestClass]
-    public class UnitTest1
+    public class SQLiteTesteUnidade
     {
+
+        string titulo = "TITULO",
+                autor = "AUTOR",
+                editora = "EDITORA",
+                publicado = "2022",
+                quantidade = "1",
+                descricao = "DESCRICAO",
+                capa = "",
+                pesquisa = "TITULO",
+                atributo = "titulo";
+
+        SQLite banco = new SQLite();
+        DataTable busca = new DataTable();
+
         [TestMethod]
-        public void TestMethod1()
+        public void TestInserirLivro()
         {
+            bool resultado = banco.InserirLivro(titulo, autor, editora, publicado, quantidade, descricao, capa);
+            Assert.IsTrue(resultado);
+
+            if (resultado)
+            {
+                busca = banco.ProcurarLivro(pesquisa, atributo);
+            }
+
+            if (resultado)
+            {
+                banco.ExcluirLivro(busca.Rows[0][0].ToString());
+            }
+        }
+
+        [TestMethod]
+        public void TestProcurarLivro()
+        {
+            bool resultado = banco.InserirLivro(titulo, autor, editora, publicado, quantidade, descricao, capa);
+
+            if (resultado)
+            {
+                busca = banco.ProcurarLivro(pesquisa, atributo);
+            }
+
+            Assert.IsTrue(busca.Rows.Count > 0);
+
+            if (resultado)
+            {
+                banco.ExcluirLivro(busca.Rows[0][0].ToString());
+            }
+
+        }
+
+        [TestMethod]
+        public void TestExcluirLivro()
+        {
+            bool resultado = banco.InserirLivro(titulo, autor, editora, publicado, quantidade, descricao, capa);
+
+            if (resultado)
+            {
+                busca = banco.ProcurarLivro(pesquisa, atributo);
+            }
+
+            if (resultado)
+            {
+                banco.ExcluirLivro(busca.Rows[0][0].ToString());
+            }
+
+            if (resultado)
+            {
+                busca = banco.ProcurarLivro(pesquisa, atributo);
+                Assert.IsFalse(busca.Rows.Count > 0);
+            }
+
         }
     }
 }
